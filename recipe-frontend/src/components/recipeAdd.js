40ebe08829fa42fe930e1dd555474376ff888a28
maxdickinson2 <max17dickinson@hotmail.com>
2023-03-14 15:53:23 -0700

    max finsih css for recipeadd

--- recipe-frontend/src/components/recipeAdd.js
+++ recipe-frontend/src/components/recipeAdd.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
 function AddRecipe({ update, setUpdate }) {
   const [picture, setPicture] = useState("");
   const [title, setTitle] = useState("");
   const [recipe, setRecipe] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,41 +22,56 @@ function AddRecipe({ update, setUpdate }) {
       setPicture("");
       setTitle("");
       setRecipe("");
+      setShowForm(false);
     });
   };
 
+  const handleCancel = () => {
+    setShowForm(false);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Add a Recipe</h2>
-      <label>
-        Picture:
-        <input
-          type="text"
-          value={picture}
-          onChange={(e) => setPicture(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Title:
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Recipe:
-        <textarea
-          value={recipe}
-          onChange={(e) => setRecipe(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Add Recipe</button>
-    </form>
+    <div className={`add-recipe-container ${showForm ? 'expanded' : ''}`}>
+      {showForm ? (
+        <form onSubmit={handleSubmit}>
+          <h2>Add a Recipe</h2>
+          <label className="image">
+            Picture:
+            <input
+              className="recipeInput"
+              type="text"
+              value={picture}
+              onChange={(e) => setPicture(e.target.value)}
+              required
+            />
+          </label>
+          <label className="recipeTitle">
+            Title:
+            <input
+              className="recipeInput"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </label>
+          <label className="recipeInfo">
+            Recipe:
+            <textarea
+              className="recipeInput"
+              value={recipe}
+              onChange={(e) => setRecipe(e.target.value)}
+              required
+            />
+          </label>
+          <button className="addRecipeBtn" type="submit">Add Recipe</button>
+          <button className="cancelRecipeBtn" type="button" onClick={handleCancel}>Cancel</button>
+        </form>
+      ) : (
+        <button className="addRecipeBtn" onClick={() => setShowForm(true)}>Add Recipe</button>
+      )}
+    </div>
   );
 }
 
-export default AddRecipe
+export default AddRecipe;
